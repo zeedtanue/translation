7c40e64e7cb33c867718b9c3df7a1832b2225712
zeedtanue <zeedtanue@gmail.com>
2022-01-24 04:01:15 +0600

    some basic css

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,27 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <div className='header'>
+        <select name="" id="">
+          <option value="">Bangla</option>
+          <option value="">English</option>
+
+        </select>
+        <div className='inputContainer'>
+            <textarea 
+              
+              placeholder="Your text to translate..."
+              name='text'
+            />
+          </div>
+          <select name="" id="">
+          <option value="">Bangla</option>
+          <option value="">English</option>
+
+        </select>
+          <button >Translate</button>
+        </div>
+      <div className='translation'></div>
     </div>
   );
 }
